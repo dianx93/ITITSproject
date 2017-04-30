e986ca796f2d6f63fd2c5613b3b28bbf826fa6c6
AndreasGP <aaatttsss@hotmail.com>
2017-04-30 11:08:58 +0300

    Added a readme, refactored image reader

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ITITSproject
-Traffic sign detection.
+
+##Setup
+
+To run this, you need to have OpenCV 3.2.0+ and scikit-learn installed.
+
+Next, go to http://benchmark.ini.rub.de/?section=gtsrb&subsection=dataset#Downloads and download Training set's and Test set's images and HOG features.
+
+Copy the 'Images' folder from the downloaded training images to the 'train' folder (Create the folder at root if it does not exist).
+
+Copy the 'Images' folder from the downloaded testing images to the 'test' folder (Create the folder at root if it does not exist).
+
+Copy the 'HOG' folder from the downloaded training HOG features to the 'train' folder.
+
+Copy the 'HOG' folder from the downloaded testing HOG features to the 'test' folder.
--- Reader.py
+++ Reader.py
@@ -1,12 +1,8 @@
-import os
-import cv2
+import os, random, cv2
 import numpy as np
 from image_classification import classification
 
-path = "GTSRB/Final_Training/Images/"
-
-images = {}
-
+#Extracts the image metadata from the .csv files (Bounding box of the traffic sign)
 def add_image_details(file):
     f = open(file)
     data = f.read().split("\n")[1:-1]
@@ -21,25 +17,29 @@ def add_image_details(file):
         images[name]["Y2"] = int(Y2)
         images[name]["CID"] = int(CID)
 
-for folder in os.listdir(path):
-    folder_index = int(folder)
-    sign_type = classification[folder_index]
-    for file in os.listdir(path + folder):
-        if file.endswith("ppm"):
-            images[file] = {"IMAGE":cv2.imread(path + folder+ "/" +file)}
-            images[file]["SIGNTYPE"] = sign_type
-        else:
-            add_image_details(path + folder+ "/" +file)
+if __name__ == "__main__":
+    path = "train/Images/"
+    images = {}
 
+    for folder in os.listdir(path):
+        folder_index = int(folder)
+        sign_type = classification[folder_index]
+        for file in os.listdir(path + folder):
+            if file.endswith("ppm"):
+                #Read the .ppm file containing the image
+                images[file] = {"IMAGE": cv2.imread(path + folder + "/" + file)}
+                images[file]["SIGNTYPE"] = sign_type
+            elif file.endswith("csv"):
+                #Read the .csv file containing the bounding boxes of traffic signs
+                add_image_details(path + folder + "/" + file)
 
-import random
-image = random.choice(images.keys())
+    image = random.choice(images.keys())
 
-img = images[image]
-cv2.rectangle(img["IMAGE"],(img["X1"],img["Y1"]),(img["X2"],img["Y2"]),(255,0,0),3)
-cv2.imshow("Test",img["IMAGE"])
+    img = images[image]
+    cv2.rectangle(img["IMAGE"],(img["X1"],img["Y1"]),(img["X2"],img["Y2"]),(255,0,0),3)
+    cv2.imshow("Test",img["IMAGE"])
 
-while True:
-    k=cv2.waitKey(1) & 0xFF
-    if k== 27: break
-cv2.destroyAllWindows()
+    while True:
+        k=cv2.waitKey(1) & 0xFF
+        if k== 27: break
+    cv2.destroyAllWindows()
