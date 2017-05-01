21c2d2382bf7595662e1d492b64d6e4c18029fd8
AndreasGP <aaatttsss@hotmail.com>
2017-05-01 18:41:18 +0300

    Refactored most of SPV class. Tri detection now checks that one triangle side is roughly parallel to the x axis

--- SinglePixelVoting.py
+++ SinglePixelVoting.py
@@ -3,20 +3,24 @@ import cv2, math
 
 #For reference, see chapter 2.1 of http://perso.lcpc.fr/tarel.jean-philippe/publis/jpt-icpr10.pdf
 class SinglePixelVoting:
-	
-	def getTrafficSigns(image, minSize):
-		minRectSize = minSize
+
+	#TODO: Finish this method
+	def getTrafficSigns(self, image, minSize, maxSize):
+
 		redSignsColor = [255, 255, 0]
 		blueSignsColor = [0, 255, 255]
 		
 		possibleSigns = []
-		redMask = spv.getRedMask(image, 0.65, 1.03).astype(np.uint8)
+
+		# Optimal parameters for finding everything: 0.65 and 1.03
+		redMask = self.getRedMask(image, 0.65, 1.03).astype(np.uint8)
 		im2,redContours,_ = cv2.findContours(redMask,1,2)
-		redContours = removeBadContours(redContours, minRectSize)
+
+		redContours = self.removeBadContours(image, redContours, minSize, maxSize)
+
 		#drawRectsOnImage(image, redContours, minRectSize, redSignsColor)
-		image = cv2.drawContours(image, redContours, -1, redSignsColor, 1)
-		
-		#TODO move methods to class, get list of possible traffic signs
+
+		#cv2.drawContours(image, redContours, -1, redSignsColor, 1)
 		return possibleSigns
 		
 	#Extracts the red part of the image with some tresholding.
@@ -54,6 +58,109 @@ class SinglePixelVoting:
 							mask[y][x] = 255
 
 		return mask
+
+	# Removes all the contours that are smaller than the given size.
+	def removeBadContours(self, image, mask, contours, minSize, maxSize):
+		out = []
+
+		for contour in contours:
+			x, y, w, h = cv2.boundingRect(contour)
+			# Simple size check
+			if w < minSize or h < minSize or w > maxSize or h > maxSize:
+				continue
+
+			#Calculate the convex hull of the contour
+			hull = cv2.convexHull(contour)
+			hullLen = cv2.arcLength(hull, True)
+
+			if (self.isTriangularSign(image, contour, hull, hullLen)
+				or self.isCircularSign(image, mask, contour)):
+				out.append(contour)
+
+		return out
+
+	def isTriangularSign(self, image, contour, hull, hullLen):
+		#TODO: Optimize the step size and bounds, our goal is to find
+		# the approximation with length 3 and make sure we don't miss it
+		#Possible solution: When we reach 2, take a half step back, if still nothing found, stop.
+		for i in np.arange(0.06, 0.2, 0.02):
+			epsilon = i * hullLen
+			approx = cv2.approxPolyDP(hull, epsilon, True)
+			l = len(approx)
+			#print(str(i) + " " + str(l))
+			if l < 3:
+				#No good results :(
+				break
+			elif l == 3:
+				#We have found a triangle approximation for this contour
+				#See if all the contour angles are within our limits
+				#and one triangle side is roughly parallel to the x axis
+				minAngle = 40
+				maxAngle = 85
+				#how many degs can the side roughly parallel to the x axis be rotated in either way
+				rotationError = 20
+				anglesOk = True
+				oneParallelSide = False
+
+				for i in range(0, l):
+					p1 = tuple(approx[i][0])
+					p2 = approx[(i + 1) % l][0]
+					p3 = approx[(i + 2) % l][0]
+
+					ang = angle(p1, p2, p3)
+
+					if ang < minAngle or ang > maxAngle:
+						anglesOk = False
+						break
+
+					slopeAng = math.degrees(math.atan2(p2[0] * 1.0 - p1[0], p2[1] * 1.0 - p1[1]))
+					#Normalize to [0, 180)
+					if slopeAng < 0:
+						slopeAng += 180
+
+					if(slopeAng > 90 - rotationError and slopeAng < 90 + rotationError):
+						#The line is roughly parallel to the x axis
+						if oneParallelSide is False:
+							oneParallelSide = True
+						else:
+							#One other side was already found to be roughly parallel to
+							#the x axis, abort
+							anglesOk = False
+							oneParallelSide = False
+							break
+
+				#if all angles were within limits, draw the triangle
+				if anglesOk and oneParallelSide:
+					for i in range(0, l):
+						p1 = tuple(approx[i][0])
+						p2 = approx[(i + 1) % l][0]
+						cv2.line(image, tuple(p1), tuple(p2),  [0, 255, 0], 1)
+
+				return anglesOk
+		return True
+
+	def isCircularSign(self, image, mask, contour):
+		x, y, w, h = cv2.boundingRect(contour)
+		offset = 30
+
+		minX = max(0, x-offset)
+		minY = max(0, y-offset)
+		maxX = min(x+w+offset, image.shape[1] - 1)
+		maxY = min(y+h+offset, image.shape[0] - 1)
+
+		#print(str(minX) + " - " + str(maxX) + " " + str(minY) + " - " + str(maxY))
+		#Cut a rectangular region around the contour
+		subimage = mask[minY:maxY, minX:maxX]
+
+		# param1 - it is the higher threshold of the two passed to the Canny() edge detector (the lower one is twice smaller).
+		# param2 - it is the accumulator threshold for the circle centers at the detection stage. The smaller it is, the more
+		# false circles may be detected. Circles, corresponding to the larger accumulator values, will be returned first.
+		circles = cv2.HoughCircles(subimage, cv2.HOUGH_GRADIENT, 0.5, 10, param1=20, param2=14, minRadius=5,
+								   maxRadius=30)
+		if circles is not None:
+			#print "Adding " + str(len(circles[0, :])) + " circles"
+			for circle in circles[0, :]:
+				cv2.circle(image, (int(minX + circle[0]), int(minY + circle[1])), circle[2], [0, 0, 255], 1)
 
 
 #Draws rectangles on the image. Does not draw rectangles with width/height smaller than minSize or rectangles
@@ -108,115 +215,45 @@ def isContained(rect, others):
 			return True
 	return False
 
-#Removes all the contours that are smaller than the given size and is convex.
-def removeBadContours(contours, minSize):
-	out = []
+if __name__ == "__main__":
+	spv = SinglePixelVoting()
 
-	for contour in contours:
-		x, y, w, h = cv2.boundingRect(contour)
-		#Simple size check
-		if w < minSize or h < minSize:
-			continue
+	#images = { 9 }
+	images = { 2, 4, 5, 6, 7, 8, 9, 10, 11 }
+	showImages = False
+	saveToFiles = True
 
-		#TODO: move the angle validation stuff here at some point
+	minRectSize = 10
+	maxRectSize = 50
+	redSignsColor = [255, 255, 0]
+	blueSignsColor = [0, 255, 255]
 
-		out.append(contour)
+	for imageId in images:
+		print "Image " + str(imageId)
+		image = cv2.imread("streets/" + str(imageId) + ".png")
 
-	return out
+		redMask = spv.getRedMask(image, 0.65, 1.03).astype(np.uint8)
+		tresh = cv2.cvtColor(redMask, cv2.COLOR_GRAY2BGR)
+		im2,redContours,_ = cv2.findContours(redMask,1,2)
+		redContours = spv.removeBadContours(image, redMask, redContours, minRectSize, maxRectSize)
+		#drawRectsOnImage(image, redContours, minRectSize, redSignsColor)
+		#image = cv2.drawContours(image, redContours, -1, redSignsColor, 1)
 
-spv = SinglePixelVoting()
+		#blueMask = spv.getBlueMask(image, 0.7, 1, 0.075, 0.4).astype(np.uint8)
+		#tresh = cv2.cvtColor(blueMask, cv2.COLOR_GRAY2BGR)
+		#cv2.imshow("Test", tresh)
+		#im2, blueContours, _ = cv2.findContours(blueMask, 1, 2)
+		#drawRectsOnImage(image, blueContours, minRectSize, blueSignsColor)
 
-#images = { 2 }
-images = { 2, 4, 5, 6, 7, 8, 9, 10, 11 }
-showImages = False
-saveToFiles = True
+		if saveToFiles:
+			cv2.imwrite("output/" + str(imageId) + ".png", image)
 
-minRectSize = 10
-redSignsColor = [255, 255, 0]
-blueSignsColor = [0, 255, 255]
+		#if showImages:
+			#cv2.imshow("Test",image)
 
-for imageId in images:
-	print "Image " + str(imageId)
-	image = cv2.imread("streets/" + str(imageId) + ".png")
-
-	#Optimal parameters for finding everything: 0.65 and 1.03
-	redMask = spv.getRedMask(image, 0.65, 1.03).astype(np.uint8)
-	tresh = cv2.cvtColor(redMask, cv2.COLOR_GRAY2BGR)
-	im2,redContours,_ = cv2.findContours(redMask,1,2)
-	redContours = removeBadContours(redContours, minRectSize)
-	#drawRectsOnImage(image, redContours, minRectSize, redSignsColor)
-	image = cv2.drawContours(image, redContours, -1, redSignsColor, 1)
-
-	for contour in redContours:
-		#1. Calculate convex hull of the contour
-		hull = cv2.convexHull(contour)
-		hullLen = cv2.arcLength(hull, True)
-
-		#TODO: Optimize the step size and bounds, our goal is
-		# to find the approximation with length 3 and make sure we don't miss it
-		for i in np.arange(0.02, 1, 0.02):
-			epsilon = i * hullLen
-			approx = cv2.approxPolyDP(hull, epsilon, True)
-			l = len(approx)
-			if l < 3:
-				#No good results :(
+	if showImages:
+		while True:
+			k=cv2.waitKey(1) & 0xFF
+			if k== 27:
 				break
-			elif l == 3:
-				#We have found a triangle approximation for this contour
-
-				#See if all the contour angles are within our limits
-				minAngle = 35
-				maxAngle = 85
-				anglesOk = True
-
-				for i in range(0, l):
-					p1 = tuple(approx[i][0])
-					p2 = approx[(i + 1) % l][0]
-					p3 = approx[(i + 2) % l][0]
-
-					ang = angle(p1, p2, p3)
-
-					if ang < minAngle or ang > maxAngle:
-						anglesOk = False
-						break
-
-				#If all angles were within limits, draw the triangle
-				if anglesOk:
-					for i in range(0, l):
-						p1 = tuple(approx[i][0])
-						p2 = approx[(i + 1) % l][0]
-						cv2.line(image, tuple(p1), tuple(p2),  [0, 255, 0], 1)
-
-				break
-
-	#Run circle detection on the image
-	image_grey = cv2.cvtColor(tresh, cv2.COLOR_BGR2GRAY)
-	#cv2.imwrite("output/test.png", image_grey)
-
-	#param1 - it is the higher threshold of the two passed to the Canny() edge detector (the lower one is twice smaller).
-	#param2 - it is the accumulator threshold for the circle centers at the detection stage. The smaller it is, the more
-	# false circles may be detected. Circles, corresponding to the larger accumulator values, will be returned first.
-	circles = cv2.HoughCircles(image_grey, cv2.HOUGH_GRADIENT, 0.5, 10, param1=20, param2=14, minRadius= 5, maxRadius=30)
-	if circles is not None:
-		print "Adding " + str(len(circles[0, :])) + " circles"
-		for circle in circles[0, :]:
-			cv2.circle(image, (circle[0], circle[1]), circle[2], [0, 0, 255], 1)
-
-	#blueMask = spv.getBlueMask(image, 0.7, 1, 0.075, 0.4).astype(np.uint8)
-	#tresh = cv2.cvtColor(blueMask, cv2.COLOR_GRAY2BGR)
-	#cv2.imshow("Test", tresh)
-	#im2, blueContours, _ = cv2.findContours(blueMask, 1, 2)
-	#drawRectsOnImage(image, blueContours, minRectSize, blueSignsColor)
-
-	if saveToFiles:
-		cv2.imwrite("output/" + str(imageId) + ".png", image)
-
-	#if showImages:
-		#cv2.imshow("Test",image)
-
-if showImages:
-	while True:
-		k=cv2.waitKey(1) & 0xFF
-		if k== 27:
-			break
-cv2.destroyAllWindows()
+	cv2.destroyAllWindows()
